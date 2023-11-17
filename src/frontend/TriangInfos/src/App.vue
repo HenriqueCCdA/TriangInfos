ed2ca25f27133b3b8e66f1236c82b3f64bf0a9c8
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-11-17 03:11:08 -0300

    [FEAT: #4] - Iniciar o front.
    close #4

---
+++ src/frontend/TriangInfos/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import FormVue from '@/components/FormVue.vue'
+import { useStore } from '@/stores/core'
+
+const store = useStore()
+</script>
+
+<template>
+  <v-container>
+    <h1 class="title">Lados do Triangulo</h1>
+
+    <FormVue />
+
+    <div v-if="store.area" class="mt-5 expressao">Valor da area: {{ store.area }}</div>
+
+    <v-alert v-if="store.msg_error" type="error" class="mt-5">
+      {{ store.msg_error }}
+    </v-alert>
+  </v-container>
+</template>
+
+<style scoped>
+.expressao {
+  margin-bottom: 16px;
+  font-size: 1.25rem;
+  text-align: center;
+  border-radius: 2px;
+  background-color: rgb(115, 170, 152);
+}
+
+.title {
+  text-align: center;
+}
+</style>
---
+++ src/frontend/TriangInfos/src/components/FormVue.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from 'vue'
+import { useStore } from '@/stores/core'
+
+let a = ref(null)
+let b = ref(null)
+let c = ref(null)
+
+async function calcular() {
+  const store = useStore()
+
+  const options = {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ a: a.value, b: b.value, c: c.value })
+  }
+
+  store.area = null
+  store.msg_error = null
+
+  try {
+    const response = await fetch('http://127.0.0.1:8000/area', options)
+
+    const body = await response.json()
+
+    if (!response.ok) {
+      throw new Error(body.detail)
+    }
+
+    store.area = parseFloat(body.area).toFixed(2)
+  } catch (error) {
+    store.msg_error = error
+  }
+}
+</script>
+
+<template>
+  <v-form @submit.prevent="calcular">
+    <v-text-field type="number" v-model="a" placeholder="Lado a" required />
+
+    <v-text-field type="number" v-model="b" placeholder="Lado b" required />
+    <v-text-field type="number" v-model="c" placeholder="Lado c" required />
+    <v-btn variant="tonal" type="submit" block>Calcular</v-btn>
+  </v-form>
+</template>
+
+<style scoped></style>
