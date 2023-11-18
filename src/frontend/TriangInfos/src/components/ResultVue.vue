a2c45db89bf5fa2c8fa2465a7515a5431c112c8d
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-11-18 00:15:54 -0300

    [REFAC: #6] - Extraindo mais componentes.
    close #6

---
+++ src/frontend/TriangInfos/src/components/ResultVue.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useStore } from '@/stores/core'
+const store = useStore()
+</script>
+
+<template>
+    <div v-if="store.area" class="mt-5 expressao">Valor da area: {{ store.area }}</div>
+</template>
+
+<style scoped>
+.expressao {
+  margin-bottom: 16px;
+  font-size: 1.25rem;
+  text-align: center;
+  border-radius: 2px;
+  background-color: rgb(115, 170, 152);
+}
+
+</style>
