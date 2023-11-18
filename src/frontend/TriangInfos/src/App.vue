e980bea07f204beea8d05f42b321d0b0f559492f
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-11-18 00:15:54 -0300

    [REFAC: #6] - Extraindo mais componentes.
    close #6

--- src/frontend/TriangInfos/src/App.vue
+++ src/frontend/TriangInfos/src/App.vue
@@ -1,8 +1,7 @@
 <script setup>
 import FormVue from '@/components/FormVue.vue'
-import { useStore } from '@/stores/core'
-
-const store = useStore()
+import ResultVue from './components/ResultVue.vue'
+import ErrorVue from './components/ErrorVue.vue'
 </script>
 
 <template>
@@ -10,23 +9,14 @@ const store = useStore()
     <h1 class="title">Lados do Triangulo</h1>
 
     <FormVue />
+    <ResultVue/>
+    <ErrorVue/>
 
-    <div v-if="store.area" class="mt-5 expressao">Valor da area: {{ store.area }}</div>
-
-    <v-alert v-if="store.msg_error" type="error" class="mt-5">
-      {{ store.msg_error }}
-    </v-alert>
   </v-container>
 </template>
 
 <style scoped>
-.expressao {
-  margin-bottom: 16px;
-  font-size: 1.25rem;
-  text-align: center;
-  border-radius: 2px;
-  background-color: rgb(115, 170, 152);
-}
+
 
 .title {
   text-align: center;
---
+++ src/frontend/TriangInfos/src/components/ErrorVue.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useStore } from '@/stores/core'
+const store = useStore()
+</script>
+
+<template>
+  <v-alert v-if="store.msg_error" type="error" class="mt-5">
+    {{ store.msg_error }}
+  </v-alert>
+</template>
+
+<style scoped></style>
